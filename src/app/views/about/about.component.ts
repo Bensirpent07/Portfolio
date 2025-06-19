27335e71cfc44f70c7daf6d915e99c8368632dbd
Bensirpent07 <benkuhman@gmail.com>
2025-06-19 01:55:01 -0400

    Built out about me page

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NavbarComponent} from '../../components/navbar/navbar.component';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faCheckCircle, faCode, faDownload, faJetFighterUp, faRocket} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
@@ -16,4 +17,9 @@ export class AboutComponent {
 
   protected readonly faLinkedin = faLinkedin;
   protected readonly faGithub = faGithub;
+  protected readonly faCheckCircle = faCheckCircle;
+  protected readonly faDownload = faDownload;
+  protected readonly faRocket = faRocket;
+  protected readonly faJetFighterUp = faJetFighterUp;
+  protected readonly faCode = faCode;
 }
